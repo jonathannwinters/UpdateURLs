ad33d96caca8a32fe2693403581faf389afe5517
jonathannwinters <jonathan@winters.im>
2018-08-03 01:11:16 -0400

    Update UpdateURLs.py

--- UpdateURLs.py
+++ UpdateURLs.py
@@ -5,7 +5,7 @@ __author__  = "Jonathan N. Winters"
 __email__ = "jnw25@cornell.edu"
 __version__ = "0.1"
 __license__ = "GPL"
-__description__ = "This Python script accepts three arguments, a given directory, text to search for, and the text replacement.  It will recusively go through every HTML,  HTM, PY and PHP, RB file in the given directory and subdirectory and replace the search text with the given replacement text. "
+__description__ = "This Python script accepts three arguments; a given directory, text to search for, and the text replacement and will recusively go through every HTML,  HTM, PY and PHP, RB file in the given directory and subdirectory and replace the search text with the given replacement text. "
 
 import sys, os
 # set variables given as arguments
